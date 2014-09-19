8c780d06598d12dc5fbebf9d5a6ebcc12cc9fdd6
bohnen <bohnen.net@gmail.com>
2014-09-19 17:41:44 +0900

    exam 7

--- learnyounode/mkmodule_m.js
+++ learnyounode/mkmodule_m.js
@@ -1,17 +1,11 @@
 module.exports = function ls (dir,ext,callback){
 	var fs = require('fs');
 	var path = require('path');
-	var dirs = new Array();
+	var dirs = [];
 
 	fs.readdir(dir,function(err,list){
 		if(err)
 			return callback(err,dirs);
-		// list.forEach(function(data){
-		// 	if(path.extname(data) === ("." + ext)){
-		// 		dirs.push(data);
-		// 	}
-		// });
-		// callback(null,dirs);
 		list.forEach(function(data){
 			list = list.filter(function(data){
 				return path.extname(data) === '.' + ext;
@@ -19,4 +13,4 @@ module.exports = function ls (dir,ext,callback){
 		});
 		callback(null,list);
 	});
-}
+};
