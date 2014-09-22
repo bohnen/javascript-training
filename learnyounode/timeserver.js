ba59887ce62a326c81cd90f60400875a4fa7db22
bohnen <bohnen.net@gmail.com>
2014-09-22 23:50:43 +0900

    comment

--- learnyounode/timeserver.js
+++ learnyounode/timeserver.js
@@ -2,6 +2,7 @@ var net = require('net');
 var strftime = require('strftime');
 var server = net.createServer(function(socket){
     var date = strftime("%Y-%m-%d% %H:%M%n",new Date());
+    // socket writeもある
     socket.end(date);
 });
 server.listen(Number(process.argv[2]));
